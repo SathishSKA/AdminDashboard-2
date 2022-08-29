cd38466de55c91fe45ed22ae890e1de86997f944
SathishSKA <vpssurya7@gmail.com>
2022-08-29 14:43:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+import { Drawer1 } from "./components/Drawer1";
+
+export function App() {
+  return (
+    <div className="App">
+      <Drawer1 />
+    </div>
+  );
+}
+
+export default App;
